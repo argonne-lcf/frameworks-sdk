00b9df4acdd272ecccffa9d57413024566d7f740
Nathan Nichols <8723338+nscottnichols@users.noreply.github.com>
2026-02-02 23:08:10 +0000

    remove comment

--- tests/single_node/functionality/vllm/xpu-model-inspection-hidden-sigsegv/repro_vllm_inspect_llama.py
+++ tests/single_node/functionality/vllm/xpu-model-inspection-hidden-sigsegv/repro_vllm_inspect_llama.py
@@ -7,7 +7,7 @@ os.environ["VLLM_CACHE_ROOT"] = tempfile.mkdtemp(prefix="vllm_cache_")
 import vllm.model_executor.models.registry as r
 
 ARCH = "LlamaForCausalLM"
-model_obj = r.ModelRegistry.models[ARCH]   # <- this exists in your env
+model_obj = r.ModelRegistry.models[ARCH]
 
 print("Model object:", type(model_obj), model_obj)
 print("About to call _try_inspect_model_cls…")

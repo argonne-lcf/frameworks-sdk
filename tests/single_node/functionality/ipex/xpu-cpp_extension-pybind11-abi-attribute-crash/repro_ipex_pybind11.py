b7fb2aa547c04c348031f5d6338223745cdfef2b
Nathan S. Nichols <8723338+nscottnichols@users.noreply.github.com>
2026-02-10 17:08:01 -0600

    add test to compile using IPEX XPU JIT extension (#14)
    * add test to compile using IPEX XPU JIT extension

---
+++ tests/single_node/functionality/ipex/xpu-cpp_extension-pybind11-abi-attribute-crash/repro_ipex_pybind11.py
@@ -0,0 +1,39 @@
+import os
+import torch
+from intel_extension_for_pytorch.xpu import cpp_extension as xpu_ext
+
+print("torch:", torch.__version__)
+print("has _PYBIND11_COMPILER_TYPE:", hasattr(torch._C, "_PYBIND11_COMPILER_TYPE"))
+
+cpp = r"""#include <torch/extension.h>
+
+torch::Tensor add(torch::Tensor a, torch::Tensor b) {
+    return a + b;
+}
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+    m.def("add", &add, "add");
+}
+"""
+
+
+src_path = os.path.join(os.getcwd(), "pyb11_repro.cpp")
+with open(src_path, "w") as f:
+    f.write(cpp)
+
+build_dir = os.path.join(os.getcwd(), "build_repro")
+os.makedirs(build_dir, exist_ok=True)
+
+mod = xpu_ext.load(
+    name="pyb11_repro",
+    sources=[src_path],
+    extra_cflags=["-O0"],
+    build_directory=build_dir,
+    verbose=True,
+    is_python_module=True,
+    keep_intermediates=True,
+)
+
+print("Built module:", mod)
+print("OK:", mod.add(torch.ones(1), torch.ones(1)))
+

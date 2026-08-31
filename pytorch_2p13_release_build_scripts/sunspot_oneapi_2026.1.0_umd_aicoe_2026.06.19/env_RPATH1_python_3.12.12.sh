85cb92eeef4a55f5a2b5bd3f73f55968a9621b97
Khalid Hossain <khossain4337@gmail.com>
2026-08-31 20:56:19 +0000

    Added a environment script to test RPATHs

---
+++ pytorch_2p13_release_build_scripts/sunspot_oneapi_2026.1.0_umd_aicoe_2026.06.19/env_RPATH1_python_3.12.12.sh
@@ -0,0 +1,144 @@
+#!/bin/bash -x
+#
+# Time Stamp
+tstamp() {
+     date +"%Y-%m-%d-%H%M%S"
+}
+## Proxies to clone from a compute node
+export HTTP_PROXY=http://proxy.alcf.anl.gov:3128
+export HTTPS_PROXY=http://proxy.alcf.anl.gov:3128
+export http_proxy=http://proxy.alcf.anl.gov:3128
+#
+CONDA_ENV_INSTALL_DIR=/lus/tegu/projects/datasets/software/26.181.0/wheelforge/envs/conda_envs
+CONDA_ENV_NAME=RP1_RC4_python_3.12.12
+
+WHEELHOUSE_TMP=/lus/tegu/projects/datasets/software/26.181.0/wheelhouse_cleanup
+WHEELHOUSE_DOWNLOAD=/lus/tegu/projects/datasets/software/26.181.0/wheelforge/downloaded_wheels
+PYTORCH_REPO_DIR=/lus/tegu/projects/datasets/software/26.181.0/wheelforge/repositories/pytorch_2.13.0_07_21_2026/pytorch
+TMPDIR_FOR_ENVS=/lus/tegu/projects/datasets/software/26.181.0/wheelforge/envs/tmpdir_for_envs
+
+module add miniforge3/25.11.0-1
+source $MINIFORGE3_ROOT/bin/activate
+# The line above does exactly the line below
+#source /opt/aurora/26.181.0/spack/unified/1.1.1/install/linux-x86_64/miniforge3-25.11.0-1-khkcc6i/bin/activate
+
+ENVPREFIX=$CONDA_ENV_INSTALL_DIR/$CONDA_ENV_NAME
+CONDA_ENV_MANIFEST=${CONDA_ENV_INSTALL_DIR}/manifests/${CONDA_ENV_NAME}
+
+rm -rf ${ENVPREFIX}
+mkdir -p ${ENVPREFIX}
+
+rm -rf ${CONDA_ENV_MANIFEST}
+mkdir -p ${CONDA_ENV_MANIFEST}
+
+export CONDA_PKGS_DIRS=${ENVPREFIX}/../.conda/pkgs
+export PIP_CACHE_DIR=${ENVPREFIX}/../.pip
+
+echo "Creating Conda environment with Python 3.12.12"
+conda create python=3.12.12 icu=73 glog=0.4.0 gflags=2.2.2 fmt --prefix ${ENVPREFIX} --override-channels \
+           --channel conda-forge \
+           --strict-channel-priority \
+           --yes
+
+conda activate ${ENVPREFIX}
+echo "Conda is coming from $(which conda)"
+
+# Use default modules on Aurora with oneapi/2026.1.0 with PTI 0.17.0
+# Use umd_aicoe_2026.06.19
+ml add intel_gpu_umd_aicoe/2026.06.19
+module add cmake
+unset CMAKE_ROOT
+module add ninja
+module add pti-gpu
+module add hdf5
+
+## CHANGE HERE!!!
+TMP_WORK=${TMPDIR_FOR_ENVS}
+cd $TMP_WORK
+
+mkdir -p ${CONDA_ENV_NAME}
+
+LOG_FILE=${TMP_WORK}/${CONDA_ENV_NAME}/module-$(tstamp).log
+
+touch ${LOG_FILE}
+module -t list 2>&1 | tee ${LOG_FILE}
+
+pip install conda-pack ipython uv
+pip install --no-deps --no-build-isolation --find-links ${WHEELHOUSE_TMP} -r ${WHEELHOUSE_TMP}/RC4_all_wheels.list
+pip install --no-deps --no-build-isolation -r ${WHEELHOUSE_TMP}/RC4_all_nowheels.list
+pip install --no-deps --no-build-isolation --no-cache-dir --force-reinstall -r ${WHEELHOUSE_TMP}/RC4_all_wheels.list 
+
+set +e
+
+rm_conda_pkgs=(
+        "dpcpp-cpp-rt"
+        "impi_rt"
+        "intel-cmplr-lib-rt"
+        "intel-cmplr-lib-ur"
+        "intel-cmplr-lic-rt"
+        "intel-gpu-ocl-icd-system"
+        "intel-opencl-rt"
+        "intel-openmp"
+        "intelpython"
+        "intel-sycl-rt"
+        "level-zero"
+        "libedit"
+        "mkl"
+        "mkl-include"
+        "mkl_fft"
+        "mkl_random"
+        "mkl-service"
+        "mkl_umath"
+        "onemkl-sycl-blas"
+        "onemkl-sycl-dft"
+        "onemkl-sycl-lapack"
+        "onemkl-sycl-rng"
+        "onemkl-sycl-stats"
+        "onemkl-sycl-vm"
+        "pyedit"
+        "tbb"
+        "tcm"
+        "umf"
+        "tcmlib"
+        "intel-pti"
+        "impi-rt"
+        "oneccl"
+        "oneccl-devel"
+        "onemkl-sycl-sparse"
+        "nvidia-nccl-cu12"
+        "mlflow-tracing"
+        "onemkl-license"
+        "mkl-static"
+        "libgcc"
+        "libgcc-ng"
+        "libgomp"
+        "liblzma"
+        "libnsl"
+        "libstdcxx"
+        "libstdcxx-ng"
+        "libuuid"
+        "libxcrypt"
+        "ncurses"
+        "ld_impl_linux-64"
+        "icu"
+    )
+
+for pkg in "${rm_conda_pkgs[@]}"
+do
+    conda uninstall $pkg \
+        --prefix ${ENVPREFIX} \
+        --force \
+        --yes
+    pip uninstall $pkg -y
+done
+
+echo ""
+echo "Writing the package lists"
+conda list > $CONDA_ENV_MANIFEST/${CONDA_ENV_NAME}_conda_env.list 2>&1
+pip list > $CONDA_ENV_MANIFEST/${CONDA_ENV_NAME}_pip.list 2>&1
+echo "Package list writing finished"
+
+echo "Writing $CONDA_ENV_MANIFEST/${CONA_ENV_NAME}_all.list"
+cat $CONDA_ENV_MANIFEST/${CONDA_ENV_NAME}_conda_env.list | grep -v '^#' | grep 'pypi$' | perl -pe 's/^(\S+)\s+(\S+).*/$1==$2/' >  $CONDA_ENV_MANIFEST/${CONDA_ENV_NAME}_all.list
+
+ 
